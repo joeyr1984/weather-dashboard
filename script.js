70fadd1b4d6706ce0404b05cb6bbb8b0e2773943
Joey Reyes <joeyr1984@gmail.com>
2020-05-15 19:28:10 -0700

    fixed today and five day forecast

--- script.js
+++ script.js
@@ -1,34 +1,84 @@
 var cityName, todaysForecast, dayOne, dayTw0, dayThree, dayFour, dayFive;
 
-
+const fiveDayForecastApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const currentWeatherForecastApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const units = "imperial";
+const apiKey = "59cf0d27eea117f08fb58b5f7644dbcd";
 $(document).ready(function () {
-    const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
-    const units = "imperial";
-    const apiKey = "59cf0d27eea117f08fb58b5f7644dbcd";
-
-    function searchForCity(cityName) {
-        $.ajax({
-            url: apiUrl + cityName + '&units=' + units + '&appid=' + apiKey,
-            method: 'GET'
-        }).then(function (response) {
-            console.log(response);
-            cityName = response.city.name;
-            todaysForecast = response.list[0];
-            dayOne = response.list[1];
-            dayTw0 = response.list[2];
-            dayThree = response.list[3];
-            dayFour = response.list[4];
-            dayFive = response.list[5];
-
-            updateTodaysForecast(cityName, todaysForecast);
-        });
-    }
-    searchForCity('sedona');
+   searchTodaysForecast('sedona');
+   searchFiveDayForecast('sedona');
 });
-
-function updateTodaysForecast(city, weather) {
-    $('#city-name').html(city + ' (' + moment(new Date(weather.dt * 1000)).format('MM/DD/YYYY') + ') <img src="http://openweathermap.org/img/wn/' + weather.weather[0].icon + '.png"/>');
+function searchTodaysForecast(cityName) {
+    $.ajax({
+        url: currentWeatherForecastApiUrl + cityName + '&units=' + units + '&appid=' + apiKey,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        
+        updateTodaysForecast(response);
+        
+    });   
+}
+function searchFiveDayForecast(cityName) {
+    $.ajax({
+        url: fiveDayForecastApiUrl + cityName + '&units=' + units + '&appid=' + apiKey,
+        method: 'GET'
+    }).then(function (response) {
+        //console.log(response);
+        dayOne = response.list[3];
+        dayTwo = response.list[11];
+        dayThree = response.list[19];
+        dayFour = response.list[27];
+        dayFive = response.list[35];
+        updateFiveDayForecast(dayOne, dayTwo, dayThree, dayFour, dayFive);
+    });
+}
+function updateTodaysForecast(weather) {
+    console.log(weather);
+    $('#city-name').html(weather.name + ' (' + moment(new Date(weather.dt * 1000)).format('MM/DD/YYYY') + ') <img src="http://openweathermap.org/img/wn/' + weather.weather[0].icon + '.png"/>');
     $('#temp').html('Temperature: ' + weather.main.temp + ' °F');
     $('#humidity').html('Humidity: ' + weather.main.humidity + ' %');
     $('#wind-speed').html('Wind Speed: ' + weather.wind.speed + ' MPH');
+}
+function updateFiveDayForecast(one, two, three, four, five) {
+    //DAY ONE FORECAST
+    $('#day-one-date').text(moment(new Date(one.dt * 1000)).format('MM/DD/YYYY'));
+    $('#day-one-icon').attr({
+        src: 'http://openweathermap.org/img/wn/' + one.weather[0].icon + '.png', 
+        alt: one.weather[0].description
+    })
+    $('#day-one-temp').text('Temp: ' + one.main.temp + ' °F');
+    $('#day-one-humidity').text('Humidity: ' + one.main.humidity + ' %');
+    //DAY TWO FORECAST
+    $('#day-two-date').text(moment(new Date(two.dt * 1000)).format('MM/DD/YYYY'));
+    $('#day-two-icon').attr({
+        src: 'http://openweathermap.org/img/wn/' + two.weather[0].icon + '.png', 
+        alt: two.weather[0].description
+    })
+    $('#day-two-temp').text('Temp: ' + two.main.temp + ' °F');
+    $('#day-two-humidity').text('Humidity: ' + two.main.humidity + ' %');
+    //DAY THREE FORECAST
+    $('#day-three-date').text(moment(new Date(three.dt * 1000)).format('MM/DD/YYYY'));
+    $('#day-three-icon').attr({
+        src: 'http://openweathermap.org/img/wn/' + three.weather[0].icon + '.png', 
+        alt: three.weather[0].description
+    })
+    $('#day-three-temp').text('Temp: ' + three.main.temp + ' °F');
+    $('#day-three-humidity').text('Humidity: ' + three.main.humidity + ' %');
+    //DAY FOUR FORECAST
+    $('#day-four-date').text(moment(new Date(four.dt * 1000)).format('MM/DD/YYYY'));
+    $('#day-four-icon').attr({
+        src: 'http://openweathermap.org/img/wn/' + four.weather[0].icon + '.png', 
+        alt: four.weather[0].description
+    })
+    $('#day-four-temp').text('Temp: ' + four.main.temp + ' °F');
+    $('#day-four-humidity').text('Humidity: ' + four.main.humidity + ' %');
+    //DAY FIVE FORECST
+    $('#day-five-date').text(moment(new Date(five.dt * 1000)).format('MM/DD/YYYY'));
+    $('#day-five-icon').attr({
+        src: 'http://openweathermap.org/img/wn/' + five.weather[0].icon + '.png', 
+        alt: five.weather[0].description
+    })
+    $('#day-five-temp').text('Temp: ' + five.main.temp + ' °F');
+    $('#day-five-humidity').text('Humidity: ' + five.main.humidity + ' %');
 }
